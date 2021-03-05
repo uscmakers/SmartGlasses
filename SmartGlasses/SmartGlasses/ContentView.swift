7855aa5f031c88a57222091ce5c3b978f66fe110
Devin Mui <devinwmui@gmail.com>
2021-03-04 19:08:15 -0800

    init

---
+++ SmartGlasses/SmartGlasses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartGlasses
+//
+//  Created by Devin Mui on 3/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartGlasses/SmartGlasses/SmartGlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartGlassesApp.swift
+//  SmartGlasses
+//
+//  Created by Devin Mui on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SmartGlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
