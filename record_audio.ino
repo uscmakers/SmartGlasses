1b61a69e52e9173f7f4cbff730e75e9e8b3952b5
bcinbis <31314514+bcinbis@users.noreply.github.com>
2021-04-23 23:28:00 -0600

    Update record_audio.ino

--- record_audio.ino
+++ record_audio.ino
@@ -1,12 +1,3 @@
-/*
-Steps:
-1. Edit pcmConfig.h
-    a: On Uno or non-mega boards, #define buffSize 128. May need to increase.
-    b: Uncomment #define ENABLE_RECORDING and #define BLOCK_COUNT 10000UL
-
-2. Usage is as below. See https://github.com/TMRh20/TMRpcm/wiki/Advanced-Features#wiki-recording-audio for
-   additional informaiton.
-*/
 
 #include <SD.h>
 #include <SPI.h>
@@ -35,30 +26,21 @@ void setup() {
 
   Serial.begin(115200);
 
-  //Serial.write("START\r\n");
-
-
 }
 
 void loop() {
   
   if(toSend) {
-    //Serial.println("recording saved");
 
     File dataFile = SD.open("AUDIO.WAV");
     // if the file is available, write to it:
     if (dataFile) {
       
-      //Serial.write("START\r\n");
       digitalWrite(4, HIGH);
       delay(100);
-      
-      
-
       while (dataFile.available()) {
         Serial.write(dataFile.read());
       }
-      //Serial.println("END");
       dataFile.close();
       digitalWrite(4, LOW);
     }
@@ -70,24 +52,19 @@ void loop() {
 }
 
 void button() {
-  //Serial.println("starting 1");
   while (i < 300000) {
     i++;
   }
 
-  //Serial.println("starting 2");
   i = 0;
   if (recmode == 0) {
     recmode = 1;
-    //audiofile++;
-    //digitalWrite(13, HIGH);
-    
+
     audio.startRecording("audio.wav", 32000, A0);
       
   }
   else {
     recmode = 0;
-    //digitalWrite(13, LOW);
     audio.stopRecording("audio.wav");
     toSend = true;
     
